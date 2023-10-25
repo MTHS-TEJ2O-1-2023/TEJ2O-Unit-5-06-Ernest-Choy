24a747db822477668deb0becbf5d07af9c3dea08
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-25 12:43:10 -0400

    typever

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ernest
+ * Created on: oct 2023
+ * This program find the distance through sonar
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToTarget: number = 0
+
+// set up 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// find distance
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceToTarget - sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  basic.showNumber(distanceToTarget)
+  basic.showIcon(IconNames.Happy)
+})
